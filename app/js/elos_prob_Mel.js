395e48e6de290fdeeaf921b516e64da89b54516b
Andrew Dao <29350324+andrewwdao@users.noreply.github.com>
2022-12-21 12:07:14 +1100

    update helper function to automatically save csv file, remove redundancy dependancy and clean up

--- app/js/elos_prob_Mel.js
+++ app/js/elos_prob_Mel.js
@@ -2,15 +2,21 @@
 Cesium.Ion.defaultAccessToken =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4OGZjZGIwZC03ODlhLTRmNGEtOGMyMC1kZTRlNzI3OGFjNzkiLCJpZCI6NjQxNzYsImlhdCI6MTYyODg2MDY2M30.4Qs7AJNdHE8AA3SG6vJLhZORa6L3ln9oHwuH5NasJfw";
 
-// import * as fs from 'fs'
-// var fs = require("fs");
-
 const viewer = new Cesium.Viewer("cesiumContainer");
-
 const osmBuildingsTileset = Cesium.createOsmBuildings();
 
-viewer.scene.primitives.add(osmBuildingsTileset);
+// Buffer for saved data
+var wDat = [];
+// The position of the transmitter.
+const Tx = Cesium.Cartesian3.fromDegrees(144.956, -37.82, 25);
+//The position of the receiver.
+const Rx = Cesium.Cartesian3.fromDegrees(144.948373, -37.818445, 25);
+var objectsToExclude = [];
 
+//console.log("Tx: "+ Tx);
+//console.log("Rx: "+ Rx);
+
+viewer.scene.primitives.add(osmBuildingsTileset);
 viewer.scene.camera.setView({
   destination: Cesium.Cartesian3.fromDegrees(144.956, -37.82, 100), //The coordinates of Melbourne CBD.
   orientation: {
@@ -18,16 +24,6 @@ viewer.scene.camera.setView({
     pitch: Cesium.Math.toRadians(-30),
   },
 });
-// The position of the transmitter.
-const Tx = Cesium.Cartesian3.fromDegrees(144.956, -37.82, 25);
-
-//The position of the receiver.
-const Rx = Cesium.Cartesian3.fromDegrees(144.948373, -37.818445, 25);
-
-//console.log("Tx: "+ Tx);
-//console.log("Rx: "+ Rx);
-
-var objectsToExclude = [];
 
 //The ray between Tx and Rx is shown in red.
 objectsToExclude.push(
@@ -40,8 +36,75 @@ objectsToExclude.push(
   })
 );
 //viewer.zoomTo(viewer.entities);
-var wDat = [];
 
+/** -----------------------------------------------------------------------------------------
+ * a function to convert a row vector to a column vector
+ * (c) Tian Han 2022
+ * @param {array} grid
+ * @returns
+  -------------------------------------------------------------------------------------------
+ */
+function rows2cols(grid) {
+  let r = [];
+  for (let i = 0; i < grid.length; i++) {
+    console.log([grid[i]]);
+    r.push([grid[i]]);
+  }
+  return r;
+}
+
+/** -----------------------------------------------------------------------------------------
+ * a function for data exportation
+ * (c) Tian Han 2022
+ * @param {string} filename
+ * @param {array} rows
+  -------------------------------------------------------------------------------------------
+ */
+function exportToCsv(filename, rows) {
+  var processRow = function (row) {
+    var finalVal = "";
+    for (var j = 0; j < row.length; j++) {
+      var innerValue = row[j] === null ? "" : row[j].toString();
+      if (row[j] instanceof Date) {
+        innerValue = row[j].toLocaleString();
+      }
+      var result = innerValue.replace(/"/g, '""');
+      if (result.search(/("|,|\n)/g) >= 0) result = '"' + result + '"';
+      if (j > 0) finalVal += ",";
+      finalVal += result;
+    }
+    return finalVal + "\n";
+  };
+
+  var csvFile = "";
+  for (var i = 0; i < rows.length; i++) {
+    csvFile += processRow(rows[i]);
+  }
+
+  var blob = new Blob([csvFile], { type: "text/csv;charset=utf-8;" });
+  if (navigator.msSaveBlob) {
+    // IE 10+
+    navigator.msSaveBlob(blob, filename);
+  } else {
+    var link = document.createElement("a");
+    if (link.download !== undefined) {
+      // feature detection
+      // Browsers that support HTML5 download attribute
+      var url = URL.createObjectURL(blob);
+      link.setAttribute("href", url);
+      link.setAttribute("download", filename);
+      link.style.visibility = "hidden";
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    }
+  }
+}
+
+/** -----------------------------------------------------------------------------------------
+ * a function for checking Line of Sight between two points
+  -------------------------------------------------------------------------------------------
+ */
 function check_LOS() {
   for (let i = 0; i < 300; i++) {
     //The direction vector of the ray is calculated as "Rx-Tx".
@@ -68,13 +131,11 @@ function check_LOS() {
         (intersection.position.z - Tx.z) * (intersection.position.z - Tx.z);
       ans = Math.sqrt(ans);
 
-      // needs fs - Node.js File system Module
-      //  fs.appendFile('data.txt',ans,function (err) {
-      //     if (err) throw err;
-      //     console.log(ans);
-      //  });
-      wDat = wDat + ans.toString() + "\r\n";
+      //old method to add to a csv file
+      // wDat = wDat + ans.toString() + "\r\n";
       //  console.log(ans);
+      // new method
+      wDat.push([ans.toString()]);
 
       viewer.entities.add({
         position: intersection.position,
@@ -87,7 +148,8 @@ function check_LOS() {
   }
 
   var buf = new Blob([wDat], { type: "text/plain;charset=utf-8" });
-  saveAs(buf, "data.txt");
+  // saveAs(buf, "data.txt");
+  exportToCsv("data.csv", rows2cols(wDat));
   console.log("Data saved.");
 }
 
